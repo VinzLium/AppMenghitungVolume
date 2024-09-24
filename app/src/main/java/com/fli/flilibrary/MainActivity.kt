6a207865fb9e92d7a5b5b0ee8f647abf98e1bb4a
Fli <muharafli827@gmail.com>
2024-09-24 13:42:10 +0700

    add onSaveInstanceState

--- app/src/main/java/com/fli/flilibrary/MainActivity.kt
+++ app/src/main/java/com/fli/flilibrary/MainActivity.kt
@@ -27,6 +27,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnCalculate = findViewById(R.id.btn_calculate)
         tvResult = findViewById(R.id.tv_result)
         btnCalculate.setOnClickListener(this)
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
